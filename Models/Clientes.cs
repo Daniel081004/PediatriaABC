ba51f64c17fa923c628d303af3f89a079a6c568e
deleo <deleo@LAPTOP-1Q15UABM>
2025-04-29 13:18:19 -0600

    Paquetes, Scaffold y ya

---
+++ Models/Clientes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PediatriaABC.Models;
+
+public partial class Clientes
+{
+    public int Id { get; set; }
+
+    public string NombreTutor { get; set; } = null!;
+
+    public string NombreHijo { get; set; } = null!;
+
+    public DateOnly FechaNacimientoHijo { get; set; }
+
+    public string? Telefono { get; set; }
+
+    public string Direccion { get; set; } = null!;
+
+    public DateOnly? FechaRegistro { get; set; }
+}
---
+++ Models/Sql3775903Context.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace PediatriaABC.Models;
+
+public partial class Sql3775903Context : DbContext
+{
+    public Sql3775903Context()
+    {
+    }
+
+    public Sql3775903Context(DbContextOptions<Sql3775903Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Clientes> Clientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("user=sql3775903;password=WI71d5gHLV;server=sql3.freesqldatabase.com;database=sql3775903;port=3306", Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.5.54-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("latin1_swedish_ci")
+            .HasCharSet("latin1");
+
+        modelBuilder.Entity<Clientes>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(200)
+                .HasDefaultValueSql("'Desconocido'")
+                .HasColumnName("direccion");
+            entity.Property(e => e.FechaNacimientoHijo).HasColumnName("fechaNacimientoHijo");
+            entity.Property(e => e.FechaRegistro).HasColumnName("fechaRegistro");
+            entity.Property(e => e.NombreHijo)
+                .HasMaxLength(255)
+                .HasColumnName("nombreHijo");
+            entity.Property(e => e.NombreTutor)
+                .HasMaxLength(255)
+                .HasColumnName("nombreTutor");
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(10)
+                .HasColumnName("telefono");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
