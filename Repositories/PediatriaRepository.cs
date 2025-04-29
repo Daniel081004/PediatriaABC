baee6cc58c8b1013f04a724fef1bd043a3b0cb5b
deleo <deleo@LAPTOP-1Q15UABM>
2025-04-29 13:46:48 -0600

    Agregar Clientes

---
+++ Repositories/PediatriaRepository.cs
@@ -0,0 +1,28 @@
+﻿using PediatriaABC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PediatriaABC.Repositories
+{
+    public class PediatriaRepository
+    {
+        Sql3775903Context context = new();
+        public IEnumerable<Clientes> GetByAll()
+        {
+            return context.Clientes;
+        }
+        public Clientes? GetById(int id)
+        {
+            return context.Clientes.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Insert(Clientes cliente)
+        {
+            context.Add(cliente);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ViewModels/PediatriaViewModel.cs
@@ -0,0 +1,100 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using PediatriaABC.Models;
+using PediatriaABC.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PediatriaABC.ViewModels
+{
+    public class PediatriaViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Clientes> ListaClientes { get; set; } = new();
+        PediatriaRepository repository = new();
+        public string Vista { get; set; } = "Home";
+        public string Errores { get; set; } = "";
+        public Clientes Cliente { get; set; } = new();
+        public ICommand VerAgregarCommand { get; set; }
+        public ICommand AgregarCommand { get; set; }
+        public ICommand CancelarCommand { get; set; }
+        public PediatriaViewModel()
+        {
+            VerAgregarCommand = new RelayCommand(VerAgregar);
+            AgregarCommand = new RelayCommand(Agregar);
+            CancelarCommand = new RelayCommand(Cancelar);
+        }
+
+        private void Cancelar()
+        {
+            Vista = "Home";
+            PropertyChanged?.Invoke(this, new(nameof(Vista)));
+            PropertyChanged?.Invoke(this, new(nameof(ListaClientes)));
+        }
+        bool Validar()
+        {
+            var hoy = DateOnly.FromDateTime(DateTime.Today);
+            var hace18Años = hoy.AddYears(-18);
+            if (string.IsNullOrWhiteSpace(Cliente.NombreTutor))
+            {
+                Errores = "Escribe el nombre del tutor\n";
+            }
+            if (string.IsNullOrWhiteSpace(Cliente.NombreHijo))
+            {
+                Errores = "Escribe el nombre del hijo\n";
+            }
+            if (!string.IsNullOrWhiteSpace(Cliente.Telefono) && Regex.IsMatch(Cliente.Telefono, @"^\d{10}$"))
+            {
+                Errores = "Escribe un numero de telefono valido\n";
+            }
+            if (string.IsNullOrWhiteSpace(Cliente.Direccion))
+            {
+                Errores = "Escribe una direccion\n";
+            }
+            if(Cliente.FechaNacimientoHijo > hoy)
+            {
+                Errores = "La fecha de nacimiento no puede ser en el futuro\n";
+            }
+            if(Cliente.FechaNacimientoHijo <= hace18Años)
+            {
+                Errores = "El niño es mayor de edad\n";
+            }
+
+            if (string.IsNullOrWhiteSpace(Errores))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void Agregar()
+        {
+            if(Cliente != null)
+            {
+                if (Validar())
+                {
+                    repository.Insert(Cliente);
+                }
+            }
+        }
+
+        private void VerAgregar()
+        {
+            Vista = "Agregar";
+            Cliente = new();
+            Errores = "";
+            PropertyChanged?.Invoke(this,new(nameof(Vista)));
+            PropertyChanged?.Invoke(this, new(nameof(Cliente)));
+            PropertyChanged?.Invoke(this, new(nameof(Errores)));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
