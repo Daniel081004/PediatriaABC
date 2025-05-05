e0a9501d43daa51986f447e074d56522baa462fd
deleo <deleo@LAPTOP-1Q15UABM>
2025-05-05 13:26:22 -0600

    xd

--- Repositories/PediatriaRepository.cs
+++ Repositories/PediatriaRepository.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PediatriaABC.Repositories
 {
@@ -19,10 +21,53 @@ namespace PediatriaABC.Repositories
             return context.Clientes.FirstOrDefault(x => x.Id == id);
         }
 
-        public void Insert(Clientes cliente)
+        public void Insert(Clientes cliente, out string Error)
         {
-            context.Add(cliente);
-            context.SaveChanges();
+            if (Validar(cliente, out Error))
+            {
+                context.Add(cliente);
+                context.SaveChanges();
+            }
+        }
+
+        bool Validar(Clientes Cliente, out string Errores)
+        {
+            Errores = "";
+            var hoy = DateOnly.FromDateTime(DateTime.Today);
+            var hace18Años = hoy.AddYears(-18);
+            if (string.IsNullOrWhiteSpace(Cliente.NombreTutor))
+            {
+                Errores = "Escribe el nombre del tutor\n";
+            }
+            if (string.IsNullOrWhiteSpace(Cliente.NombreHijo))
+            {
+                Errores = "Escribe el nombre del hijo\n";
+            }
+            if (!string.IsNullOrWhiteSpace(Cliente.Telefono) && Regex.IsMatch(Cliente.Telefono, @"^\d{10}$"))
+            {
+                Errores = "Escribe un numero de telefono valido\n";
+            }
+            if (string.IsNullOrWhiteSpace(Cliente.Direccion))
+            {
+                Errores = "Escribe una direccion\n";
+            }
+            if (Cliente.FechaNacimientoHijo > hoy)
+            {
+                Errores = "La fecha de nacimiento no puede ser en el futuro\n";
+            }
+            if (Cliente.FechaNacimientoHijo <= hace18Años)
+            {
+                Errores = "El niño es mayor de edad\n";
+            }
+
+            if (string.IsNullOrWhiteSpace(Errores))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- ViewModels/PediatriaViewModel.cs
+++ ViewModels/PediatriaViewModel.cs
@@ -36,56 +36,14 @@ namespace PediatriaABC.ViewModels
             PropertyChanged?.Invoke(this, new(nameof(Vista)));
             PropertyChanged?.Invoke(this, new(nameof(ListaClientes)));
         }
-        bool Validar()
-        {
-            Errores = "";
-            var hoy = DateOnly.FromDateTime(DateTime.Today);
-            var hace18Años = hoy.AddYears(-18);
-            if (string.IsNullOrWhiteSpace(Cliente.NombreTutor))
-            {
-                Errores = "Escribe el nombre del tutor\n";
-            }
-            if (string.IsNullOrWhiteSpace(Cliente.NombreHijo))
-            {
-                Errores = "Escribe el nombre del hijo\n";
-            }
-            if (!string.IsNullOrWhiteSpace(Cliente.Telefono) && Regex.IsMatch(Cliente.Telefono, @"^\d{10}$"))
-            {
-                Errores = "Escribe un numero de telefono valido\n";
-            }
-            if (string.IsNullOrWhiteSpace(Cliente.Direccion))
-            {
-                Errores = "Escribe una direccion\n";
-            }
-            if(Cliente.FechaNacimientoHijo > hoy)
-            {
-                Errores = "La fecha de nacimiento no puede ser en el futuro\n";
-            }
-            if(Cliente.FechaNacimientoHijo <= hace18Años)
-            {
-                Errores = "El niño es mayor de edad\n";
-            }
 
-            if (string.IsNullOrWhiteSpace(Errores))
-            {
-                return true;
-            }
-            else
-            {
-                PropertyChanged?.Invoke(this, new(nameof(Errores)));
-                return false;
-            }
-        }
         private void Agregar()
         {
-            if(Cliente != null)
+            if (Cliente != null)
             {
-                if (Validar())
-                {
-                    repository.Insert(Cliente);
-                    Vista = "Home";
-                    PropertyChanged?.Invoke(this,new(nameof(Vista)));
-                }
+                repository.Insert(Cliente, Errores);
+                Vista = "Home";
+                PropertyChanged?.Invoke(this, new(nameof(Vista)));
             }
         }
 
